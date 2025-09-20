f2944d160c6211042591f7e52a21be999a0433cd
vijay-talsangi <vijaytalsangi4705@gmail.com>
2025-09-21 02:26:17 +0530

    Implement auth

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import { signIn, signOut, signup } from '#controllers/auth.controller.js';
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/sign-up', signup);
+
+router.post('/sign-in', signIn);
+
+router.post('/sign-out', signOut);
+
+export default router;
