250410466b2314b815a902a3ba66f5a3aede5452
vijay-talsangi <vijaytalsangi4705@gmail.com>
2025-09-21 00:39:02 +0530

    Implemented Eslint and Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("This is CCD CCA 2");
+  res.status(200).send('This is CCD CCA 2');
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
+  console.log(`listening on http://localhost:${PORT}`);
 });
